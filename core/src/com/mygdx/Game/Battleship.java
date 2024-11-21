f4145b8324fbefee83b265f16081da719d625144
Parichay Sachdev <parichay.sachdev04@outlook.com>
2024-11-21 17:29:26 +0100

    gradle setup

---
+++ core/src/com/mygdx/Game/Battleship.java
@@ -0,0 +1,21 @@
+package com.mygdx.Game;
+
+import com.badlogic.gdx.Game;
+
+public class Battleship extends Game {
+
+	@Override
+	public void create () {
+
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+	
+	@Override
+	public void dispose () {
+
+	}
+}
