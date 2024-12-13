517069dbd38021c7273742e88aaae207cfe8b170
Parichay Sachdev <parichay.sachdev04@outlook.com>
2024-12-13 01:16:17 +0100

    updated board class

--- core/src/com/mygdx/Board.java
+++ core/src/com/mygdx/Board.java
@@ -1,5 +1,16 @@
 package com.mygdx;
 
+import com.badlogic.gdx.math.Rectangle;
+
 public class Board {
 
+    private final Rectangle[][] grid;
+
+    public Board(int rows, int cols) {
+        grid = new Rectangle[rows][cols];
+    }
+
+    //public getBoardPiece() {
+
+    //}
 }
